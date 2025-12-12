d1eaf4e5fbce66a307492f679648fa49d52292ee
Arseny <0notcoder@gmail.com>
2025-12-13 02:07:58 +0300

    finished ExpressionParser.

--- expressionSolver/expressionSolver.hpp
+++ expressionSolver/expressionSolver.hpp
@@ -3,45 +3,58 @@
 #include <vector>
 
 extern const enum class tokenType;
+extern const enum class symbolType;
+
+inline int getNumberFromChar(const char &symbol);
 
 struct Token {
 private:
-	int value;
+	double value;
 	tokenType type;
 
 public:
-	Token(int value, tokenType type) : value(value), type(type) {};
+	Token(double value, tokenType type) : value(value), type(type) {};
 
-	int getValue() const;
+	double getValue() const;
 	tokenType getType() const;
+
+	static symbolType typifySymbol(const char &symbol);
 };
 
 class ExpressionParser {
-	int buff;
+private:
+	double buff;
 	std::string expression;
 	std::string::iterator currentSymbolIt;
+	std::vector<Token> tokens;
 	
 	int overPoint;
+
+	inline void addToken(const Token &token);
+	char getCurrentSymbol() const;
+	void shiftCurrentSymbol();
+	void numberToken();
+	char getCurrentSymbolShift();
+	void oneSymbolToken();
+	void endState();
+
+	void (ExpressionParser:: *getNextMethod())();
 
 public:
 	ExpressionParser() : buff(0), overPoint(0) {};
 
-	static std::vector<Token> parse(const std::string &expression);
-
-	char getCurrentSymbol() const;
-	void shiftCurrentSymbol();
-	char getCurrentSymbolShift();
-
-	void numberToken();
+	std::vector<Token> parse(const std::string &expression);
 };
 
 class ExpressionSolver {
 private:
 	std::string expression;
 	std::string::iterator currentIt;
-public:
-	static bool checkEnd(const std::string &str, const std::string::iterator &strIt);
-	double solve(const std::string &expression);
+	
 	double firstPrior(const double &left);
 	double secondPrior(const double& left);
+	static bool checkEnd(const std::string &str, const std::string::iterator &strIt);
+
+public:
+	double solve(const std::string &expression);
 };
--- expressionSolver/main.cpp
+++ expressionSolver/main.cpp
@@ -5,9 +5,17 @@
 int main(int argc, char** argv) {
 	ExpressionSolver solver;
 	std::string exp;
-	std::cin >> exp;
+	std::cin >> exp; // TODO: remove spaces
 
-	std::cout << solver.solve(exp) << std::endl;
+	//std::cout << solver.solve(exp) << std::endl;
+
+	ExpressionParser parser;
+
+	std::vector<Token> tokens = parser.parse(exp);
+
+	for (std::vector<Token>::const_iterator it = tokens.begin(); it != tokens.end(); it++) {
+		std::cout << "Type: " << static_cast<int>(it->getType()) << "\nValue: " << it->getValue() << "\n\n";
+	}
 
 	return 0;
 }
